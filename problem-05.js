c1cf4776bf1d39423caed0c3b6d0ca30011a6638
lisan <mdsainulislamlisan@gmail.com>
2025-08-03 20:50:03 +0600

    problem-05 solved

---
+++ problem-05.js
@@ -0,0 +1,9 @@
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhanku" , roll: 1014 ,department: "cse" };
+//write your code here
+
+var studentName = student.name;
+var studentRoll =student.roll;
+var departmentName = student.department;
+
+console.log(studentName + studentRoll + "." + departmentName + "@ph.ac.bd");
